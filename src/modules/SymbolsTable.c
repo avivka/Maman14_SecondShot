39e6c34fa22caba1509b7b1070133291269efd8a
Amit <amitginat23@gmail.com>
2019-08-25 12:11:53 +0300

    fixed address value in the symbol table

--- src/modules/SymbolsTable.c
+++ src/modules/SymbolsTable.c
@@ -42,6 +42,8 @@ void addSymbolToTable(Symbol *newSymbol){
         ERROR_PROGRAM(("Requested label %s, already exists, can't declare the same label twice.", newSymbol->label));
     }
 
+	printf("check important %s %d \n", newSymbol-> label, newSymbol-> address);
+
     /** if no tail, it means we are setting the first item in the table */
     if(tableTail == NULL){
         tableHead = tableTail = newSymbol;
